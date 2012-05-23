3c29cbcb56a3cfa503fe8f2c95bf287874f7df5f
ktc <ktc@statsbiblioteket.dk>
2012-05-23 13:30:22 +0200

    Changes in accordance to review

--- httpresolver/src/main/java/dk/statsbiblioteket/mediaplatform/httpresolver/HttpResolverService.java
+++ httpresolver/src/main/java/dk/statsbiblioteket/mediaplatform/httpresolver/HttpResolverService.java
@@ -3,6 +3,8 @@ package dk.statsbiblioteket.mediaplatform.httpresolver;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.GET;
@@ -14,15 +16,25 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.StreamingOutput;
 
+
+/**
+ * Class to provide look up capabilities for a file in the media collection bitrepository and stream the file to the caller. 
+ * The class provides two webservice calls. 
+ * First a getfile, that requests the bitrepository for the given file and waits for an incoming stream. 
+ * Second a uploadProxy call to receive the input stream and pipe to a waiting getfile.
+ * 
+ * 
+ * 
+ */
 @Path("/resolver")
 public class HttpResolverService {
 
-    private Map<String, InputStream> connectionMapping;
-
-    public HttpResolverService() {
-        connectionMapping = ConnectionMapperFactory.getConnectionMapping();
-    }
+    private static Map<String, InputStream> connectionMapping = 
+            Collections.synchronizedMap(new HashMap<String, InputStream>());
     
+    /**
+     * Method to request the delivery of a file from the bitrepository, blocking until a inputstream becomes available 
+     */
     @GET
     @Path("/getfile/")
     @Produces(MediaType.APPLICATION_OCTET_STREAM)
@@ -56,6 +68,13 @@ public class HttpResolverService {
         };
     }
     
+    /**
+     *  Method to receive a requested file via an input stream. 
+     *  Adds its input steam to the connection mapping, notifies threads waiting for the input stream
+     *  and waits to the transfer is finished.
+     *  @param id The id of the file transfer. 
+     *  @param is The input stream providing the data that is to be transfered.  
+     */
     @PUT
     @Path("/uploadProxy/{ID}")
     public void uploadFile(@PathParam("ID") String id, InputStream is) throws InterruptedException {
