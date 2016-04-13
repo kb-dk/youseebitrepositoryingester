5db243f8169ab9bc0321a5a97648521806950034
ktc <ktc@statsbiblioteket.dk>
2016-04-13 14:18:16 +0200

    Restructuring and addition of javadoc

--- src/main/java/dk/statsbiblioteket/medieplatform/bitrepository/ingester/FilePutter.java
+++ src/main/java/dk/statsbiblioteket/medieplatform/bitrepository/ingester/FilePutter.java
@@ -1,120 +1,63 @@
 package dk.statsbiblioteket.medieplatform.bitrepository.ingester;
 
-import dk.statsbiblioteket.medieplatform.bitrepository.ingester.ClientExitCodes.ExitCodes;
+import java.net.URL;
+
 import org.bitrepository.bitrepositoryelements.ChecksumDataForFileTYPE;
 import org.bitrepository.bitrepositoryelements.ChecksumSpecTYPE;
 import org.bitrepository.bitrepositoryelements.ChecksumType;
 import org.bitrepository.common.settings.Settings;
-import org.bitrepository.common.settings.SettingsProvider;
-import org.bitrepository.common.settings.XMLFileSettingsLoader;
 import org.bitrepository.common.utils.Base16Utils;
 import org.bitrepository.common.utils.CalendarUtils;
-import org.bitrepository.modify.ModifyComponentFactory;
 import org.bitrepository.modify.putfile.PutFileClient;
-import org.bitrepository.protocol.messagebus.MessageBus;
-import org.bitrepository.protocol.messagebus.MessageBusManager;
-import org.bitrepository.protocol.security.BasicMessageAuthenticator;
-import org.bitrepository.protocol.security.BasicMessageSigner;
-import org.bitrepository.protocol.security.BasicOperationAuthorizor;
-import org.bitrepository.protocol.security.BasicSecurityManager;
-import org.bitrepository.protocol.security.MessageAuthenticator;
-import org.bitrepository.protocol.security.MessageSigner;
-import org.bitrepository.protocol.security.OperationAuthorizor;
-import org.bitrepository.protocol.security.PermissionStore;
-import org.bitrepository.protocol.security.SecurityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Properties;
-
-import javax.jms.JMSException;
+import dk.statsbiblioteket.medieplatform.bitrepository.ingester.ClientExitCodes.ExitCodes;
 
 /**
  * Class handling putting of a file this includes
- * - Loading of settings from configuration directory
  * - Validation of FileID
- * - Connection to the Bitrepository 
  * - Calls to the Bitrepository reference client and handling of events.   
  */
 public class FilePutter {
-
-    private final static String CLIENT_ID_PROPERTY 
-        = "dk.statsbiblioteket.medieplatform.bitrepository.clientid";
-    private final static String CLIENT_CERTIFICATE_PROPERTY 
-        = "dk.statsbiblioteket.medieplatform.bitrepository.clientcertificatefile";
-    private final static String BASE_URL_PROPERTY 
-        = "dk.statsbiblioteket.medieplatform.bitrepository.baseurl";
-    private final static String COLLECTION_ID_PROPERTY 
-        = "dk.statsbiblioteket.medieplatform.bitrepository.collectionid";
-    
     private final Logger log = LoggerFactory.getLogger(getClass());
     
-    private final String fileID;
     private final String collectionID;
-    private final URL fileURL;
-    private final String checksum;
-    private final long fileSize;
     private final Settings settings;
-    private final Properties properties;
-    
     private final PutFileClient putFileClient;
     
     /**
-     * Constructor. Validates inputs as part of the construction process. 
-     * @throws ClientFailureException in case of illegal inputs. 
+     * Constructor.  
+     * @param putClient The {@link PutFileClient} to use for putting files
+     * @param settings The {@link Settings} for the bitrepository
+     * @param collectionID The ID of the collection to put files into. 
      */
-    public FilePutter(String configDir, Properties properties, String fileID, String url, String checksum, 
-            long fileSize) throws ClientFailureException {
-        SettingsProvider settingsLoader = new SettingsProvider(new XMLFileSettingsLoader(configDir),
-                properties.getProperty(CLIENT_ID_PROPERTY));
-        settings = settingsLoader.getSettings();
-        this.properties = properties;
-        verifyProperties();
-        this.fileID = fileID;
-        collectionID = properties.getProperty(COLLECTION_ID_PROPERTY);
+    public FilePutter(PutFileClient putClient, Settings settings, String collectionID) {
+        this.settings = settings;
+        this.collectionID = collectionID;
+        putFileClient = putClient;
+    }
+    
+    /**
+     * Blocking call to the put file client. 
+     * @param fileID The ID of the file in the repository
+     * @param fileLocation The URL of the location where the file to be put can be found
+     * @param checksum The checksum of the file to be put
+     * @param fileSize The size of the file to be put
+     * @throws ClientFailureException in case of failure 
+     */
+    public void putFile(String fileID, URL fileLocation, String checksum, long fileSize) throws ClientFailureException {
+        PutFileEventHandler handler = new PutFileEventHandler();
+        ChecksumDataForFileTYPE checksumData = createChecksumData(checksum);
+        
         if(!fileID.matches(settings.getRepositorySettings().getProtocolSettings().getAllowedFileIDPattern())) {
             throw new ClientFailureException("The fileID is not allowed. FileID must match: " + 
                     settings.getRepositorySettings().getProtocolSettings().getAllowedFileIDPattern(), 
                     ExitCodes.ILLEGAL_FILEID);
         }
         
-        try {
-            fileURL = new URL(url);
-        } catch (MalformedURLException e) {
-            throw new ClientFailureException("Malformed URL for filelocation: " + url, ExitCodes.URL_ERROR);
-        }
-        
-        this.checksum = checksum;
-        this.fileSize = fileSize;
-        
-        PermissionStore permissionStore = new PermissionStore();
-        MessageAuthenticator authenticator = new BasicMessageAuthenticator(permissionStore);
-        MessageSigner signer = new BasicMessageSigner();
-        OperationAuthorizor authorizer = new BasicOperationAuthorizor(permissionStore);
-        SecurityManager securityManager = new BasicSecurityManager(settings.getRepositorySettings(), 
-                configDir + "/" + properties.getProperty(CLIENT_CERTIFICATE_PROPERTY),
-                authenticator, signer, authorizer, permissionStore, properties.getProperty(CLIENT_ID_PROPERTY));
-        putFileClient = ModifyComponentFactory.getInstance().retrievePutClient(settings, securityManager, 
-                properties.getProperty(CLIENT_ID_PROPERTY));
-    }
-    
-    /**
-     * Blocking call to the put file client. 
-     * @throws ClientFailureException in case of failure 
-     */
-    public void putFile() throws ClientFailureException {
-        PutFileEventHandler handler = new PutFileEventHandler();
-        ChecksumDataForFileTYPE checksumData = new ChecksumDataForFileTYPE();
-        checksumData.setChecksumValue(Base16Utils.encodeBase16(checksum));
-        checksumData.setCalculationTimestamp(CalendarUtils.getNow());
-        ChecksumSpecTYPE checksumSpec = new ChecksumSpecTYPE();
-        checksumSpec.setChecksumType(ChecksumType.MD5);
-        checksumData.setChecksumSpec(checksumSpec);
-
-        
-        putFileClient.putFile(collectionID, fileURL, fileID, fileSize, checksumData, null, handler, "Initial ingest of file");
+        putFileClient.putFile(collectionID, fileLocation, fileID, fileSize, checksumData, null, handler, 
+                "Initial ingest of file");
         
         try {
             handler.waitForFinish();
@@ -128,41 +71,18 @@ public class FilePutter {
     }
     
     /**
-     * Method to shutdown the client properly  
+     * Method to build the Bitrepository.org {@link ChecksumDataForFileTYPE} datastructure
+     * @param checksum The checksum to put into the datastructure
+     * @return {@link ChecksumDataForFileTYPE} datastructure that represents the checksum 
      */
-    public void shutdown() {
-    	try {
-    	    MessageBus messageBus = MessageBusManager.getMessageBus();
-            if (messageBus != null) {
-                MessageBusManager.getMessageBus().close();
-            }
-		} catch (JMSException e) {
-			log.warn("Failed to shutdown messagebus connection", e);
-		}
-    }    
-    
-    /**
-     * Get the url to be returned to the workflow.  
-     */
-    public String getUrl() {
-        return properties.getProperty(BASE_URL_PROPERTY) + fileID;
+    protected ChecksumDataForFileTYPE createChecksumData(String checksum) {
+        ChecksumDataForFileTYPE checksumData = new ChecksumDataForFileTYPE();
+        checksumData.setChecksumValue(Base16Utils.encodeBase16(checksum));
+        checksumData.setCalculationTimestamp(CalendarUtils.getNow());
+        ChecksumSpecTYPE checksumSpec = new ChecksumSpecTYPE();
+        checksumSpec.setChecksumType(ChecksumType.MD5);
+        checksumData.setChecksumSpec(checksumSpec);
+        return checksumData;
     }
-    
-    /**
-     * Verify required properties 
-     */
-    private void verifyProperties() {
-        if(properties.getProperty(BASE_URL_PROPERTY) == null) {
-            throw new RuntimeException(BASE_URL_PROPERTY + " is null");
-        }
-        if(properties.getProperty(CLIENT_CERTIFICATE_PROPERTY) == null) {
-            throw new RuntimeException(CLIENT_CERTIFICATE_PROPERTY + " is null");
-        }
-        if(properties.getProperty(CLIENT_ID_PROPERTY) == null) {
-            throw new RuntimeException(CLIENT_ID_PROPERTY + " is null");
-        }
-        if(properties.getProperty(COLLECTION_ID_PROPERTY) == null) {
-            throw new RuntimeException(COLLECTION_ID_PROPERTY + " is null");
-        }
-    }
+
 }
--- src/main/java/dk/statsbiblioteket/medieplatform/bitrepository/ingester/Ingester.java
+++ src/main/java/dk/statsbiblioteket/medieplatform/bitrepository/ingester/Ingester.java
@@ -2,6 +2,22 @@ package dk.statsbiblioteket.medieplatform.bitrepository.ingester;
 
 import dk.statsbiblioteket.medieplatform.bitrepository.ingester.ClientExitCodes.ExitCodes;
 
+import org.bitrepository.common.settings.Settings;
+import org.bitrepository.common.settings.SettingsProvider;
+import org.bitrepository.common.settings.XMLFileSettingsLoader;
+import org.bitrepository.modify.ModifyComponentFactory;
+import org.bitrepository.modify.putfile.PutFileClient;
+import org.bitrepository.protocol.messagebus.MessageBus;
+import org.bitrepository.protocol.messagebus.MessageBusManager;
+import org.bitrepository.protocol.security.BasicMessageAuthenticator;
+import org.bitrepository.protocol.security.BasicMessageSigner;
+import org.bitrepository.protocol.security.BasicOperationAuthorizor;
+import org.bitrepository.protocol.security.BasicSecurityManager;
+import org.bitrepository.protocol.security.MessageAuthenticator;
+import org.bitrepository.protocol.security.MessageSigner;
+import org.bitrepository.protocol.security.OperationAuthorizor;
+import org.bitrepository.protocol.security.PermissionStore;
+import org.bitrepository.protocol.security.SecurityManager;
 import org.bitrepository.protocol.utils.LogbackConfigLoader;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,7 +28,11 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
+
+import javax.jms.JMSException;
 
 /**
  * The main executable class for ingesting files in a configured bit repository.
@@ -24,11 +44,20 @@ public class Ingester {
     public static final int CHECKSUM_ARG_INDEX = 3;
     public static final int FILESIZE_ARG_INDEX = 4;
     public static final String INGESTER_PROPERTIES_FILE = "ingester.properties";
+    
+    private final static String CLIENT_ID_PROPERTY 
+        = "dk.statsbiblioteket.medieplatform.bitrepository.clientid";
+    private final static String CLIENT_CERTIFICATE_PROPERTY 
+        = "dk.statsbiblioteket.medieplatform.bitrepository.clientcertificatefile";
+    private final static String BASE_URL_PROPERTY 
+        = "dk.statsbiblioteket.medieplatform.bitrepository.baseurl";
+    private final static String COLLECTION_ID_PROPERTY 
+        = "dk.statsbiblioteket.medieplatform.bitrepository.collectionid";
 
     private final static Logger log = LoggerFactory.getLogger(Ingester.class);
     
     private String confDir;
-    private String fileLocation;
+    private URL fileLocation;
     private String fileID;
     private String checksum;
     private Long filesize;
@@ -37,7 +66,7 @@ public class Ingester {
     public static void main(String[] args) {
         try {
             Ingester ingester = new Ingester(args);
-            String url = ingester.ingest(args);
+            String url = ingester.ingest();
             writeSuccessOutput(url);
             System.exit(ExitCodes.SUCCESS.getCode());
         } catch (ClientFailureException e) {
@@ -51,30 +80,75 @@ public class Ingester {
     }
     
     public Ingester(String[] args) throws ClientFailureException {
-        verifyAndLoadInputParams(args);
+        log.info("Initializing ingester with args: " + args);
+        loadAndVerifyInputParams(args);
+        setupLogging(confDir);
+        loadAndVerifyProperties(confDir);
     }
     
-    public String ingest(String[] args) throws ClientFailureException {
-        String url = null;
-        FilePutter putter = null;
+    /**
+     * Method to setup and handle the put. 
+     * @return url The url of the newly put file in the repository
+     * @throws ClientFailureException In case of failure putting the file. 
+     */
+    public String ingest() throws ClientFailureException {
+        String clientID = properties.getProperty(CLIENT_ID_PROPERTY);
+        String certificateLocation = confDir + "/" + properties.getProperty(CLIENT_CERTIFICATE_PROPERTY);
+        String baseUrl = properties.getProperty(BASE_URL_PROPERTY);
+        String collectionID = properties.getProperty(COLLECTION_ID_PROPERTY);
         
+        log.debug("Loading bitrepository settings from dir: '{}'", confDir);
+        SettingsProvider settingsLoader = new SettingsProvider(new XMLFileSettingsLoader(confDir), clientID);
+        Settings settings = settingsLoader.getSettings();
+        log.debug("Creating bitrepository client");
+        PutFileClient putFileClient = createPutClient(clientID, certificateLocation, settings);
+        
+        log.debug("Starting put of file: '{}'", fileID);
         try {
-            setupLogging(confDir);
-            properties = loadProperties(confDir);
-            log.info("Ingest of file requested: " + args);
-            log.debug("Starting client");
-            putter = new FilePutter(confDir, properties, fileID, fileLocation, checksum, filesize);  
-            putter.putFile();
-            url = putter.getUrl();
+            FilePutter putter = new FilePutter(putFileClient, settings, collectionID);  
+            putter.putFile(fileID, fileLocation, checksum, filesize);
+            return baseUrl + fileID; 
         } finally {
-            if(putter != null) {
-                log.debug("Shutting down messagebus connection");
-                putter.shutdown();
-            }
+            shutdown();
         }
-        return url;
     }
     
+    /**
+     * Helper method to obtain the {@link PutFileClient}.
+     * @param clientID The ID of the client
+     * @param certificateLocation The location of the clients certificate in the local filesystem
+     * @param settings The {@link Settings} for the Bitrepository.org client
+     * @return {@link PutFileClient} The PutFileClient 
+     */
+    private PutFileClient createPutClient(String clientID, String certificateLocation, Settings settings) {
+        PermissionStore permissionStore = new PermissionStore();
+        MessageAuthenticator authenticator = new BasicMessageAuthenticator(permissionStore);
+        MessageSigner signer = new BasicMessageSigner();
+        OperationAuthorizor authorizer = new BasicOperationAuthorizor(permissionStore);
+        SecurityManager securityManager = new BasicSecurityManager(settings.getRepositorySettings(), 
+                certificateLocation, authenticator, signer, authorizer, permissionStore, clientID);
+        return ModifyComponentFactory.getInstance().retrievePutClient(settings, securityManager, clientID);
+    }
+    
+    /**
+     * Method to shutdown the messagebus connection. 
+     */
+    private void shutdown() {
+        try {
+            MessageBus messageBus = MessageBusManager.getMessageBus();
+            if (messageBus != null) {
+                MessageBusManager.getMessageBus().close();
+            }
+        } catch (JMSException e) {
+            log.warn("Failed to shutdown messagebus connection", e);
+        }
+    }
+    
+    /**
+     * Method to write the output of program upon success. 
+     * @param url The URL for the file in the repository
+     * @throws ClientFailureException in case of failure to serialize the result
+     */
     private static void writeSuccessOutput(String url) throws ClientFailureException {
         JSONObject obj = new JSONObject();
         try {
@@ -86,15 +160,18 @@ public class Ingester {
     }
     
     /**
-     * Method to verify the input parameters
+     * Method to verify the input parameters and set the member variables. 
      * Verifies the validity of:
      * - Number of arguments 
      * - The existence and readability of the configuration directory
      * - The ability to parse the file size parameter as a long
      * - The length and content of the checksum parameter  
+     * - If the file location, is a proper URL
      * If validation fails error is printed to console and program is exited. 
+     * @param args The arguments to parse
+     * @throws ClientFailureException in case there is an issue with a parameter
      */
-    private void verifyAndLoadInputParams(String[] args) throws ClientFailureException {
+    private void loadAndVerifyInputParams(String[] args) throws ClientFailureException {
         if(args.length != 5) {
             throw new ClientFailureException("Unexpected number of arguments, got " + args.length + " but expected 5" + 
                     "Expecting: ConfigDirPath FileUrl FileID FileChecksum FileSize", 
@@ -136,10 +213,21 @@ public class Ingester {
                     " contains non hexadecimal value!", ExitCodes.CHECKSUM_ERROR);
         } 
         
-        fileLocation = args[FILE_LOCATION_ARG_INDEX];
+
+        try {
+            fileLocation = new URL(args[FILE_LOCATION_ARG_INDEX]);
+        } catch (MalformedURLException e) {
+            throw new ClientFailureException("Malformed URL for fileLocation: " + fileLocation, ExitCodes.URL_ERROR);
+        }
+        
         fileID = args[FILEID_ARG_INDEX];
     }
     
+    /**
+     * Method to handle setup of logging 
+     * @param configDir String representation of the directory to find 'logback.xml'
+     * @throws ClientFailureException in case setup of logging fails 
+     */
     private static void setupLogging(String configDir) throws ClientFailureException {
         try {
             new LogbackConfigLoader(configDir + "/logback.xml");
@@ -149,18 +237,32 @@ public class Ingester {
     }
     
     /**
-     * Load properties from configuration file 
+     * Load and verify properties from configuration file
+     * @param configDir The configuration dir to load the properties from 
      */
-    private static Properties loadProperties(String configDir) {
-        Properties properties = new Properties();
+    private void loadAndVerifyProperties(String configDir) {
+        properties = new Properties();
         try {
             String propertiesFile = configDir + "/" + INGESTER_PROPERTIES_FILE;
             BufferedReader reader = new BufferedReader(new FileReader(propertiesFile));
             properties.load(reader);
-            return properties;
+                    
+            if(properties.getProperty(BASE_URL_PROPERTY) == null) {
+                throw new RuntimeException(BASE_URL_PROPERTY + " is null");
+            }
+            if(properties.getProperty(CLIENT_CERTIFICATE_PROPERTY) == null) {
+                throw new RuntimeException(CLIENT_CERTIFICATE_PROPERTY + " is null");
+            }
+            if(properties.getProperty(CLIENT_ID_PROPERTY) == null) {
+                throw new RuntimeException(CLIENT_ID_PROPERTY + " is null");
+            }
+            if(properties.getProperty(COLLECTION_ID_PROPERTY) == null) {
+                throw new RuntimeException(COLLECTION_ID_PROPERTY + " is null");
+            }
         } catch (IOException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
+    
 }
